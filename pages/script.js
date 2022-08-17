9ab0a1606877d1de3af82e46d72d821b4843672a
Christina Gomzina <chrisgoncharova@yandex.ru>
2022-08-17 19:12:10 +0300

    feat: script for menu

--- pages/script.js
+++ pages/script.js
@@ -1,13 +1,29 @@
-let buttonArrow = document.querySelectorAll('.header__submenu-button');
+const buttonArrow = document.querySelectorAll('.header__submenu-button');
+const menu = document.querySelector('.header__menu');
+const buttonMenu = document.querySelector('.header__menu-button');
+const buttonMenuImage = document.querySelector('.header__menu-button-image');
 
+// Открытие основного меню 
+buttonMenu.addEventListener('click', function() {
+  if(getComputedStyle(menu).display == 'none') {
+    menu.style.display = 'flex';
+    buttonMenuImage.style.backgroundImage = 'url(../images/header_menu-button-close.svg)'
+  } else {
+    menu.style.display = 'none';
+    buttonMenuImage.style.backgroundImage = 'url(../images/header_menu-button.svg)'
+  }
+})
+
+// Открытие подменю по клику на стрелочку
 for(i=0; i<buttonArrow.length; i++) {
-  let submenu = buttonArrow[i].nextElementSibling;
-  let usedArrow =  buttonArrow[i];
+  const submenu = buttonArrow[i].nextElementSibling;
+  const usedArrow =  buttonArrow[i];
   buttonArrow[i].addEventListener('click', function() {
-    // submenu.classList.toggle('header__menu-opened');
-    // function changeDisplayStyle (menu) {
-    //   menu.style.display = 'block';
-    // }
+    if (getComputedStyle(submenu).display == 'none') {
+      submenu.style.display = 'block';
+    } else {
+      submenu.style.display = 'none';
+    }
     usedArrow.classList.toggle('header__submenu-button_active');
   });
 }
