f3b26f612a74b6d641d4492511fea32b382bbdcd
Ivan <foxriver660@gmail.com>
2022-08-22 16:18:05 +0300

    added scroll

--- pages/popup-support.js
+++ pages/popup-support.js
@@ -3,6 +3,9 @@ const amountBtn = document.querySelector(".js-amount-btn");
 const formAmount = document.querySelector(".js-form-amount");
 const userAmount = document.querySelector(".js-form__user-input_type_amount");
 const amount = document.querySelector(".js-amount");
+const radioTopBtns = document.querySelectorAll(".js-radio-top-amount");
+const radioBottomBtns = document.querySelectorAll(".js-radio-bottom-amount");
+const supportBtn = document.querySelector(".js-support-btn");
 function openPopup(popup) {
   popup.classList.add('form__popup_opened');
   };
@@ -16,3 +19,15 @@ const changeAmount = function (evt) {
         closePopup(amountPopup)
   };
 formAmount.addEventListener("submit", changeAmount);
+// СКРОЛЛ С ВЕРХНЕГО БЛОКА
+function chooseAmount() {
+    for (let i=0;i<radioTopBtns.length; i++) {
+    if (radioTopBtns[i].checked) {
+      radioBottomBtns[i].setAttribute("checked", "checked");
+    }
+    if(radioBottomBtns[4].checked) {
+      openPopup(amountPopup)
+    }
+  }
+}
+supportBtn.addEventListener("click", () => chooseAmount())
