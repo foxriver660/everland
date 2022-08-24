208f87e91017ab26242d2d433049dfecd19d65ef
Ivan <foxriver660@gmail.com>
2022-08-24 13:20:40 +0300

    bugfixed-refactor after rewie

--- scripts/popup-support.js
+++ scripts/popup-support.js
@@ -1,11 +1,12 @@
 const amountPopup = document.querySelector(".form__popup");
 const amountBtn = document.querySelector(".js-amount-btn");
 const formAmount = document.querySelector(".js-form-amount");
-const userAmount = document.querySelector(".js-form__user-input_type_amount");
+const userAmount = document.querySelector(".form__user-input_type_amount");
 const amount = document.querySelector(".js-amount");
 const radioTopBtns = document.querySelectorAll(".js-radio-top-amount");
 const radioBottomBtns = document.querySelectorAll(".js-radio-bottom-amount");
 const supportBtn = document.querySelector(".js-support-btn");
+const page = document.querySelector(".page");
 function openPopup(popup) {
   popup.classList.add('form__popup_opened');
   };
@@ -21,13 +22,20 @@ const changeAmount = function (evt) {
 formAmount.addEventListener("submit", changeAmount);
 // СКРОЛЛ С ВЕРХНЕГО БЛОКА
 function chooseAmount() {
-    for (let i=0;i<radioTopBtns.length; i++) {
-    if (radioTopBtns[i].checked) {
+  formAmount.scrollIntoView({
+    block: "start",
+    behavior: "smooth",
+})
+    for (let i=0;i<=radioTopBtns.length; i++) {
+    if (radioTopBtns[i].checked){
+      radioBottomBtns.forEach((item) => item.removeAttribute("checked"))
+      closePopup(amountPopup)
       radioBottomBtns[i].setAttribute("checked", "checked");
     }
     if(radioBottomBtns[4].checked) {
       openPopup(amountPopup)
     }
   }
+  
 }
 supportBtn.addEventListener("click", () => chooseAmount())
